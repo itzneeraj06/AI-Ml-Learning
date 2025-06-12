234c61406ce3720c734528467b3e1b74770ee8be
itzneeraj06 <neerajchouhan0604@gmail.com>
2025-06-12 19:06:13 +0530

    openai integration

---
+++ openai/openaiDemo.py
@@ -0,0 +1,31 @@
+# ------->this is llm model demo
+# from langchain_openai import OpenAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm = OpenAI(model='gpt-3.5-turbo-instruct')
+# result=llm.invoke("*********ask question here*********")
+# print(result)
+
+
+# --------->this is chatmodel demo
+# from langchain_openai import ChatOpenAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# llm=ChatOpenAI(model='gpt-4',temperature=0)
+# result=model.invoke('capital of the india ')
+# print(result.content)
+
+# ---------->gemini ai demo
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# model=ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+# result=model.invoke("testing purpose only")
+# print(result)
+
